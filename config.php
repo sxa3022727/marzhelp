b6d9595602750f8703fca6588906bc143b631449
Pouria <168307391+ppouria@users.noreply.github.com>
2024-10-16 15:36:08 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$botToken = '';
+$apiURL = "https://api.telegram.org/bot$botToken/";
+
+$allowedUsers = [1088532278, 987654321];
+
+$botDbHost = 'localhost';
+$botDbUser = 'root';
+$botDbPass = '';
+$botDbName = 'marzhelp';
+
+
+$vpnDbHost = ''; 
+$vpnDbPort = 3306; 
+$vpnDbUser = 'root';
+$vpnDbPass = '';
+$vpnDbName = 'marzban';
+?>
